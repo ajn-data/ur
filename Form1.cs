7f01ffcad1ea787dbb5f89ba8cc35f1a90fe8940
ajn-data <62245502+ajn-data@users.noreply.github.com>
2020-06-15 14:15:04 +0200

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Windows.Threading;
+
+namespace DigitalUr
+{
+    public partial class Form1 : Form
+    {
+        DispatcherTimer dt = new DispatcherTimer();
+        Stopwatch sw = new Stopwatch();
+        string currentTime = string.Empty;
+        private readonly object elapsedtimeitem;
+
+        public Form1()
+        {
+            InitializeComponent();
+            dt.Tick += new EventHandler(dt_Tick);
+            dt.Interval = new TimeSpan(0, 0, 0, 0, 1);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer.Start();
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            lblStatus.Text = DateTime.Now.ToString("T");
+        }
+
+        private void dato(object sender, EventArgs e)
+        {
+            lblDato.Text = DateTime.Today.ToString("T");
+        }
+
+        public void startbtn(object sender, EventArgs e)
+        {
+            sw.Start();
+            dt.Start();
+        }
+
+        private void stopbtn(object sender, EventArgs e)
+        {
+            if (sw.IsRunning)
+            {
+                sw.Stop();
+            }
+            elapsedtimeitem.Items.Add(currentTime);
+        }
+
+        void dt_Tick(object sender, EventArgs e)
+        {
+            if (sw.IsRunning)
+            {
+                TimeSpan ts = sw.Elapsed;
+                currentTime = String.Format("{0:00}:{1:00}:{2:00}",
+                ts.Minutes, ts.Seconds, ts.Milliseconds / 10);
+                lblClock.Text = currentTime;
+            }
+        }
+        private void resetbtn(object sender, EventArgs e)
+        {
+            sw.Reset();
+            lblClock.Text = "00:00:00";
+        }
+    }
+} 
+
